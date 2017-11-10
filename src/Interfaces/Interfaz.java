1429d51f7cd0ba0d718c42c80660919476366b80
alicia1984 <alicia28031@hotmail.com>
2017-11-10 19:25:58 -0300

    conexion de java con  base d e  datos mysql estructura  completa mas  la  creacion  de mis  clases  correspondiente

---
+++ src/Interfaces/Interfaz.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+import javax.swing.*;
+import java.awt.*;
+import bd.ConexionBD ;
+import java.sql.Connection;
+import java.awt.event.*;
+
+//paso 14  importar  la  clase  conexionBd
+/**
+ **Alicia Villarroel Astudillo
+ * @author LP 13
+ * Fecha:Noviembre 10 de 20017
+ * Programa: pruebajava3
+ * Responsabilidad: estructura jframe  para creacion del  tipo  conexion para  la  bd 
+ * PRUEBA TERMINADA 
+ * @author LP 13
+ */
+public class Interfaz extends JFrame{
+  JPanel panel;
+    JLabel label;
+    JTextField text;
+    JButton boton;
+    JButton boton2;
+    //paso 15 creacion  de un  objeto tipo  coxion bd
+    ConexionBD con;
+    
+      public Interfaz() 
+    {   
+    panel = new JPanel();
+    label = new JLabel();
+    text = new JTextField(20);
+    boton = new JButton();
+     boton2 = new JButton(); 
+    
+this.add(panel);
+    panel.add(label);
+    panel.add(text);
+    panel.add(boton);
+   panel.add(boton2);
+    
+    label.setText("Atributo");
+    boton.setText("Conectar");
+    boton2.setText("Desconectar");
+    panel.setBackground(Color.orange);
+    // paso  16 agregamos escuchadore al  boton
+   boton.addActionListener(new ActionListener()
+   
+   
+   {
+    public void actionPerformed(ActionEvent evt)
+    {
+    con = new ConexionBD();
+    Connection reg = con.conectar();
+    }
+   });
+   boton2.addActionListener(new ActionListener()
+   
+   
+   {
+    public void actionPerformed(ActionEvent evt)
+    {
+   con.desconectar();
+    }
+   });
+      }
+}
---
+++ src/pruebajava3/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebaJava3;
+
+
+import javax.swing.JFrame;
+import Interfaces.Interfaz;
+/**
+ ***Alicia Villarroel Astudillo
+ * @author LP 13
+ * Fecha:Noviembre 10 de 20017
+ * Programa: pruebajava3
+ * Responsabilidad: atributos  tamaño para  la ventana  en java  para  realñizar  la  coneccion  con  la bd   en  php admin
+ * atributos  de la  ventana 
+ * @author LP 13
+ * @author LP 13
+ */
+public class Conexion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Interfaz w = new  Interfaz(); 
+     w.setVisible(true);
+     w.setBounds(0, 0, 400, 400);
+     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+// TODO code application logic here
+    }
+    
+}
